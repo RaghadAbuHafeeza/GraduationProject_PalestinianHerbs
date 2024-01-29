20c6c1340752c9d9ba14127a0b2106025031cfff
RaghadAbuHafeeza <raghadabuhafeeza@gmail.com>
2024-01-29 23:24:01 +0200

    Add in Error Component

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -4,12 +4,12 @@ function Error() {
   return (
     <div className='error'>
       <div className='image'>
-        <img src='images/error.png' />
-
+        <img src='images/error.png' alt='the page you are looking for is not been found' />
       </div>
+
         <div class='text'>
           <h1>Oops!</h1>
-<h2>We can’t find the page you are looking for...</h2>
+          <h2>We can’t find the page you are looking for...</h2>
         </div>
         
     </div>
